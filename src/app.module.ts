c1ea2a5e2d49634b6459854889312d82e3690b40
hahan <hanvietha141@gmail.com>
2026-03-11 01:26:34 +0700

    connect to psql server

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'pickleball',
+      entities: [],
+      // synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
